4b16b12c31810eadda66d8155ac406aa8e830ff7
Carson Lee <leelokhin0406@gmail.com>
2024-05-20 14:44:24 +0800

    yarn init

--- app.ts
+++ app.ts
@@ -1,12 +1,42 @@
 
 import express from "express";
+import expressSession from "express-session";
+import dotenv from "dotenv";
+
+declare module "express-session" {
+    interface SessionData {
+        userId: number;
+        username: string;
+        level:string;
+    }
+}
+
+dotenv.config()
 
 const app = express();
 const PORT = 8000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+if (!process.env.SECRET)
+    throw Error("No Secret in .env");
+
+app.use(
+    expressSession({        
+        secret: process.env.SECRET,
+        saveUninitialized: true,
+        resave: true,
+    })
+);
+
+ //parsing middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//static assets
+app.use(express.static('public/'))
+app.use(express.static("uploads"))
+
+app.get("/");
+
 
 app.listen(PORT, ()=> {
   console.log(`http://localhost:${PORT}`);
