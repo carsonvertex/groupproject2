caa0f76e3269fb58f82537e5cf03f271dec28c98
Harvie <chengchifeng8@gmail.com>
2024-05-24 11:07:37 +0800

    resolve merge conflict

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,27 +1,62 @@
 // upload pictures preview
 function uploadMultiple() {
-    const fileUploaderInputs = document.querySelectorAll(".file-uploader");
-    const profilePictures = document.querySelectorAll(".profile-picture");
-  
-    for (let i = 0; i < fileUploaderInputs.length; i++) {
-      const image = fileUploaderInputs[i].files[0];
-      // check if the file selected is not an image file
-      if (!image || !image.type.includes("image")) {
-        continue;
-      }
-  
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(image);
-  
-      fileReader.onload = (fileReaderEvent) => {
-        handleImageUpload(fileReaderEvent.target.result, profilePictures[i]);
-      };
+  const fileUploaderInputs = document.querySelectorAll(".file-uploader");
+  const profilePictures = document.querySelectorAll(".profile-picture");
+
+  for (let i = 0; i < fileUploaderInputs.length; i++) {
+    const image = fileUploaderInputs[i].files[0];
+    // check if the file selected is not an image file
+    if (!image || !image.type.includes("image")) {
+      continue;
     }
+
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(image);
+
+    fileReader.onload = (fileReaderEvent) => {
+      handleImageUpload(fileReaderEvent.target.result, profilePictures[i]);
+    };
   }
-  
+}
+
 function handleImageUpload(imageData, profilePicture) {
-    profilePicture.style.backgroundImage = `url(${imageData})`;
+  profilePicture.style.backgroundImage = `url(${imageData})`;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  getProfilePics();
+});
+
+async function getProfilePics() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const username = urlParams.get("user");
+  console.log("this is username", username)
+  try {
+    const response = await fetch(`/account/getProfilePic/${username}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("Data:", data);
+
+    if (data.length > 0) {
+      console.log("this is data length", data.length)
+      // User profile pictures found, display them instead of uploadMultiple pictures
+      const profilePictures = document.querySelectorAll(".profile-picture");
+      data.forEach((pic, index) => {
+        console.log("this is pic", pic.p1)
+        console.log("this is Index",index)
+        if (profilePictures[index]) {
+          profilePictures[index].style.backgroundImage = `url(${pic.p1})`;
+        }
+      });
+    }
+  } catch (error) {
+    console.error("Error getting profile pics:", error);
   }
+}
+
+getProfilePics();
 
 //submit pictures
 
@@ -31,28 +66,3 @@ function handleImageUpload(imageData, profilePicture) {
 // });
 
 //get profile pics
-document.addEventListener("DOMContentLoaded", function () {
-  getProfilePics();
-});
-
-async function getProfilePics() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const username = urlParams.get('user');
-
-  try {
-    const response = await fetch(`/account/getProfilePic/${username}`);
-    const data = await response.json();
-    const p1 = data.p1;
-    const p2 = data.p2;
-    const p3 = data.p3;
-    const p4 = data.p4;
-    const p5 = data.p5;
-    const p6 = data.p6;
-
-    
-  } catch (error) {
-    console.error("Error getting profile pics:", error);
-  }
-}
-
-getProfilePics()
