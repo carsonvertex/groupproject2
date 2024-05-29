fc15855f0ea388a18485b08306f0a952f13515c8
Carson Lee <leelokhin0406@gmail.com>
2024-05-29 21:22:30 +0800

    test case pass

--- test/login.test.ts
+++ test/login.test.ts
@@ -1,110 +1,113 @@
-import { Client } from "pg";
-import { AccountService } from "../services/accountService";
-import { checkPassword, hashPassword } from "../utils/hash";
+import { AccountService } from '../services/accountService';
+import { Client } from 'pg';
+import * as hash from '../utils/hash';
 
-describe("AccountService", () => {
+jest.mock('pg', () => {
+  return {
+    Client: jest.fn().mockImplementation(() => {
+      return {
+        query: jest.fn(),
+      };
+    }),
+  };
+});
+
+jest.mock('../utils/hash', () => {
+  return {
+    hashPassword: jest.fn().mockResolvedValue('$2a$10$PyjdEg6CcwaExDIF6PRhHugj9cxw5uTAuQU9/sTX82rBa3nSSZjKu'),
+    checkPassword: jest.fn().mockResolvedValue(true),
+  };
+});
+
+describe('AccountService', () => {
   let accountService: AccountService;
   let mockClient: Client;
 
   beforeEach(() => {
-    mockClient = {
-      query: jest.fn(),
-    } as unknown as Client;
+    mockClient = new Client();
     accountService = new AccountService(mockClient);
   });
 
-  describe("signUp", () => {
-    it("should successfully sign up a new user", async () => {
-      const email = "test@example.com";
-      const username = "testuser";
-      const password = "testpassword";
-      const hashedPassword = await hashPassword(password);
+  describe('signUp', () => {
+    it('should successfully sign up a new user', async () => {
+      const email = 'test@example.com';
+      const username = 'testuser';
+      const password = 'testpassword';
       const returningId = { rows: [{ id: 1 }] };
 
-      (mockClient.query as jest.Mock).mockResolvedValueOnce({ rows: [] }).mockResolvedValueOnce(returningId);
+      (mockClient.query as jest.Mock).mockResolvedValueOnce({ rows: [] });
+      (mockClient.query as jest.Mock).mockResolvedValueOnce(returningId);
 
       const result = await accountService.signUp(email, username, password);
       expect(mockClient.query).toHaveBeenCalledTimes(2);
-      expect(mockClient.query).toHaveBeenNthCalledWith(1, "SELECT username,password,id FROM users WHERE email = $1 OR username = $2", [email, username]);
-      expect(mockClient.query).toHaveBeenNthCalledWith(2, "INSERT INTO users (email,username,  password) VALUES ($1, $2, $3) RETURNING id", [email, username, hashedPassword]);
-      expect(result).toEqual(returningId);
+      expect(mockClient.query).toHaveBeenNthCalledWith(1, 'SELECT username,password,id FROM users WHERE email = $1 OR username = $2', [email, username]);
+      expect(mockClient.query).toHaveBeenNthCalledWith(2, 'INSERT INTO users (email,username,  password) VALUES ($1, $2, $3) RETURNING id', [email, username, '$2a$10$PyjdEg6CcwaExDIF6PRhHugj9cxw5uTAuQU9/sTX82rBa3nSSZjKu']);
+      expect(result.rows[0]).toEqual(returningId.rows[0]);
     });
 
-    it("should throw an error for a duplicate entry", async () => {
-      const email = "test@example.com";
-      const username = "testuser";
-      const password = "testpassword";
-      const userQueryResult = { rows: [{ username: "testuser", password: "hashedpassword", id: 1 }] };
+    it('should throw an error for a duplicate entry', async () => {
+      const email = 'test@example.com';
+      const username = 'testuser';
+      const password = 'testpassword';
+      const userQueryResult = { rows: [{ username, password, id: 1 }] };
 
       (mockClient.query as jest.Mock).mockResolvedValueOnce(userQueryResult);
 
-      await expect(accountService.signUp(email, username, password)).rejects.toThrow("Duplicate entry.");
+      await expect(accountService.signUp(email, username, password)).rejects.toThrow('Duplicate entry.');
       expect(mockClient.query).toHaveBeenCalledTimes(1);
-      expect(mockClient.query).toHaveBeenCalledWith("SELECT username,password,id FROM users WHERE email = $1 OR username = $2", [email, username]);
+      expect(mockClient.query).toHaveBeenCalledWith('SELECT username,password,id FROM users WHERE email = $1 OR username = $2', [email, username]);
     });
   });
 
-  describe("logIn", () => {
-    it("should successfully log in a user", async () => {
-      const username = "testuser";
-      const password = "testpassword";
-      const hashedPassword = await hashPassword(password);
-      const userQueryResult = { rows: [{ username, password: hashedPassword, id: 1 }] };
+  describe('logIn', () => {
+    it('should successfully log in a user', async () => {
+      const username = 'testuser';
+      const password = 'testpassword';
+      const userQueryResult = { rows: [{ username, password, id: 1 }] };
 
       (mockClient.query as jest.Mock).mockResolvedValueOnce(userQueryResult);
-      (checkPassword as jest.Mock).mockResolvedValueOnce(true);
+      (hash.checkPassword as jest.Mock).mockResolvedValueOnce(true);
 
       const result = await accountService.logIn(username, password);
       expect(mockClient.query).toHaveBeenCalledTimes(1);
-      expect(mockClient.query).toHaveBeenCalledWith("SELECT username, password, id FROM users WHERE username = $1", [username]);
-      expect(checkPassword).toHaveBeenCalledWith({
-        plainPassword: password,
-        hashedPassword: hashedPassword,
-      });
+      expect(mockClient.query).toHaveBeenCalledWith('SELECT username, password, id FROM users WHERE username = $1', [username]);
       expect(result).toEqual(userQueryResult);
     });
 
-    it("should throw an error for wrong username", async () => {
-      const username = "wronguser";
-      const password = "testpassword";
-      const userQueryResult = { rows: [] };
+    it('should throw an error for wrong username', async () => {
+      const username = 'wronguser';
+      const password = 'testpassword';
 
-      (mockClient.query as jest.Mock).mockResolvedValueOnce(userQueryResult);
+      (mockClient.query as jest.Mock).mockResolvedValueOnce({ rows: [] });
 
-      await expect(accountService.logIn(username, password)).rejects.toThrow("Login Failed wrong username");
+      await expect(accountService.logIn(username, password)).rejects.toThrow('Login Failed wrong username');
       expect(mockClient.query).toHaveBeenCalledTimes(1);
-      expect(mockClient.query).toHaveBeenCalledWith("SELECT username, password, id FROM users WHERE username = $1", [username]);
+      expect(mockClient.query).toHaveBeenCalledWith('SELECT username, password, id FROM users WHERE username = $1', [username]);
     });
 
-    it("should throw an error for wrong password", async () => {
-      const username = "testuser";
-      const password = "wrongpassword";
-      const hashedPassword = await hashPassword("testpassword");
-      const userQueryResult = { rows: [{ username, password: hashedPassword, id: 1 }] };
+    it('should throw an error for wrong password', async () => {
+      const username = 'testuser';
+      const password = 'wrongpassword';
+      const userQueryResult = { rows: [{ username, password, id: 1 }] };
 
       (mockClient.query as jest.Mock).mockResolvedValueOnce(userQueryResult);
-      (checkPassword as jest.Mock).mockResolvedValueOnce(false);
+      (hash.checkPassword as jest.Mock).mockResolvedValueOnce(false);
 
-      await expect(accountService.logIn(username, password)).rejects.toThrow("Login Failed wrong password");
+      await expect(accountService.logIn(username, password)).rejects.toThrow('Login Failed wrong password');
       expect(mockClient.query).toHaveBeenCalledTimes(1);
-      expect(mockClient.query).toHaveBeenCalledWith("SELECT username, password, id FROM users WHERE username = $1", [username]);
-      expect(checkPassword).toHaveBeenCalledWith({
-        plainPassword: password,
-        hashedPassword: hashedPassword,
-      });
+      expect(mockClient.query).toHaveBeenCalledWith('SELECT username, password, id FROM users WHERE username = $1', [username]);
     });
   });
 
-  describe("users", () => {
-    it("should return all users", async () => {
-      const userQueryResult = { rows: [{ id: 1, username: "testuser1" }, { id: 2, username: "testuser2" }] };
-
-      (mockClient.query as jest.Mock).mockResolvedValueOnce(userQueryResult);
+  describe('users', () => {
+    it('should return all users', async () => {
+      const usersQueryResult = { rows: [{ username: 'user1', id: 1 }, { username: 'user2', id: 2 }] };
+      (mockClient.query as jest.Mock).mockResolvedValueOnce(usersQueryResult);
 
       const result = await accountService.users();
       expect(mockClient.query).toHaveBeenCalledTimes(1);
-      expect(mockClient.query).toHaveBeenCalledWith("SELECT * FROM users;");
-      expect(result).toEqual(userQueryResult);
+      expect(mockClient.query).toHaveBeenCalledWith('SELECT * FROM users;');
+      expect(result).toEqual(usersQueryResult);
     });
   });
 });
